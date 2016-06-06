f3ee5edacf99616b797d6b2ad5f0fb32436d15b7
davidpromacho <davidpromacho@gmail.com>
2016-06-06 23:49:20 +0200

    Clase para comprimier ficheros en un Zip

--- src/main/scala/Fichero.scala
+++ src/main/scala/Fichero.scala
@@ -1,9 +1,6 @@
-import java.io.{FileOutputStream, FileWriter}
 
 import org.apache.spark.{SparkConf, SparkContext}
-
 import scala.util.matching.Regex.Match
-
 
 @SerialVersionUID(100L)
 case class Fichero(path :String,sala: String,terminal: String, ciudad: String,socio: String, tipo: String,fecha: String)
@@ -14,9 +11,9 @@ extends Serializable{
 
 object Fichero {
 
-  val regEx = "(?:([\\w().-]+-)?(sst\\d|till\\d|sst|till)-)?([\\w]+-)?([\\w.()]+)?#(?:[0-9a-f-]{36})?([\\w]+)\\.?(\\d{4}-\\d{2}-\\d{2})?(\\.1)?\\.log(?:[\\da-f-]{36}|\\.(\\d{4}-\\d{2}-\\d{2}))?(?:[\r\n$])".r
-
-  def apply(m : Match): Fichero ={
+  val regEx = "(?:([\\w().-]+-)?(sst\\d|till\\d|sst|till)-)?([\\w]+-)?([\\w.()]+)?#(?:[0-9a-f-]{36})?([\\w]+)\\.?(\\d{4}-\\d{2}-\\d{2})?(\\.1)?\\.log(?:[\\da-f-]{36}|\\.(\\d{4}-\\d{2}-\\d{2}))?(?:[\r\n]|$)".r
+  val regEx1= "(?:([\\w().-]+-)?(sst\\d|till\\d|sst|till)-)?([\\w]+-)?([\\w.()]+)?#(?:[0-9a-f-]{36})?([\\w]+)\\.?(\\d{4}-\\d{2}-\\d{2})?(\\.1)?\\.log(?:[\\da-f-]{36}|\\.(\\d{4}-\\d{2}-\\d{2}))?(\n|$)".r
+  def apply(m : Match): Fichero = {
     new Fichero(
       path = "data/"+m.group(0).trim(),
       sala = if(m.group(1) != null) m.group(1).init else null,
@@ -25,6 +22,7 @@ object Fichero {
       socio = m.group(4),
       tipo = m.group(5),
       fecha = if(m.group(6) != null) m.group(6) else if(m.group(7) != null) m.group(7) else null
+    )
   }
 
   def readFromFile(file :String) : List[Fichero] = {
@@ -44,11 +42,12 @@ object Fichero {
     */
   def readFromFileRDD(file :String ) : List[Fichero] = {
 
-    val conf = new SparkConf().setAppName("Codere1").setMaster("local[1]")
+    val conf = new SparkConf().setAppName("Codere1").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val rdd = sc.textFile(file)
-    val rdd1 = rdd.flatMap( regEx.findAllMatchIn(_)).map( Fichero(_) )
-    return  rdd1.toLocalIterator.toList
+    val rdd1 = rdd.flatMap( regEx.findAllMatchIn(_) )
+    val rdd2 = rdd1.map( Fichero(_) )
+    return  rdd2.toLocalIterator.toList
 
   }
 
